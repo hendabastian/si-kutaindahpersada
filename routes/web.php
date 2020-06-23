976b100d2f3ae6dcd985934aed57aeff770d2556
Henda Bastian <hendabast@gmail.com>
2020-06-23 09:38:03 +0700

    show brosur & info pembangunan

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $brosur = App\Brosur::all();
+    $infoPembangunan = App\InfoPembangunan::all();
     return view('welcome', [
+        'brosur' => $brosur,
+        'infoPembangunan' => $infoPembangunan,
         'title' => 'Home | ' . env('APP_NAME')
     ]);
 });
 
 Auth::routes();
 
-Route::name('info-pembangunan-rumah.')->prefix('info-pembangunan-rumah')->middleware('auth')->group(function() {
+Route::name('info-pembangunan-rumah.')->prefix('info-pembangunan-rumah')->middleware('auth')->group(function () {
     Route::get('index', 'InformasiPembangunanRumahController@index')->name('index');
     Route::get('detail/{id}', 'InformasiPembangunanRumahController@detail')->name('detail');
     Route::get('create', 'InformasiPembangunanRumahController@create')->name('create');
@@ -30,7 +34,7 @@ Route::name('info-pembangunan-rumah.')->prefix('info-pembangunan-rumah')->middle
     Route::delete('delete/{id}', 'InformasiPembangunanRumahController@delete')->name('delete');
 });
 
-Route::name('brosur.')->prefix('brosur')->middleware('auth')->group(function() {
+Route::name('brosur.')->prefix('brosur')->middleware('auth')->group(function () {
     Route::get('index', 'BrosurController@index')->name('index');
     Route::get('detail/{id}', 'BrosurController@detail')->name('detail');
     Route::get('create', 'BrosurController@create')->name('create');
@@ -38,4 +42,13 @@ Route::name('brosur.')->prefix('brosur')->middleware('auth')->group(function() {
     Route::match(['get', 'put'], 'edit/{id}', 'BrosurController@edit')->name('edit');
     Route::delete('delete/{id}', 'BrosurController@delete')->name('delete');
 });
+
+Route::name('pemesanan.')->prefix('pemesanan')->middleware('auth')->group(function () {
+    Route::get('index', 'PemesananController@index')->name('index');
+    Route::get('detail/{id}', 'PemesananController@detail')->name('detail');
+    Route::match(['get', 'post'], 'create', 'PemesananController@create')->name('create');
+    Route::match(['get', 'put'], 'edit/{id}', 'PemesananController@edit')->name('edit');
+    Route::delete('delete/{id}', 'PemesananController@delete')->name('delete');
+});
+
 Route::get('/home', 'HomeController@index')->name('home');
